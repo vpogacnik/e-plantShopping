9c34977b24850dd57e776681ce021c4682460b78
vpogacnik <109554010+vpogacnik@users.noreply.github.com>
2025-04-03 21:26:55 -0700

    state initialization

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useSelector, useDispatch } from 'react-redux';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
+    
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
 
+    const [addedToCart, setAddedToCart] = useState({});
+    const [disabled, setDisabled] = useState(false);
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.items);
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
