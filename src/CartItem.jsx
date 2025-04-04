20d410cc961d71755b42bd526626f283ac4e8fbf
vpogacnik <109554010+vpogacnik@users.noreply.github.com>
2025-04-03 21:23:37 -0700

    handle Decrement

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -33,7 +33,18 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity(
+          { name: item.name, quantity: item.quantity - 1 }
+                      )
+      );
+    } else {
+      dispatch(
+        removeItem(
+          item.name
+                  )
+              );
   };
 
   const handleRemove = (item) => {
